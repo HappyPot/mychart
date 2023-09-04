49593a4c7c7dbdbe11fd9105886620d3ba453dfb
lin360024869 <lin360024869@devide>
2023-09-04 19:08:05 +0800

    Auto Commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'element-plus/dist/index.css'
 import router from './router';
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="wrap h-[100vw]">
-    <div class="demo">
-      <div>这是一个深色模式的切换demo</div>
-    </div>
-    <div class="nav">
-      <div class="cursor-pointer" @click="toggle()">
-        <Sunny style="width: 1.5em; height: 2em; " v-if="!isDark" />
-        <Moon style="width: 1.5em; height: 2em; " class="bg-black" v-else />
-      </div>
-    </div>
-    <div class="container">
-      <!-- 左边 -->
-      <div class="aside_left">
+  <div class="wrap">
 
+    <div class="container flex align-middle">
+      <!-- 左边 -->
+      <div class="aside_left w-[20%]">
+        <el-button :icon="Plus" class="w-[50%] m-auto" plain>New Chat</el-button>
       </div>
       <!-- 右边 -->
-      <div class="aside_right"></div>
+      <div class="aside_right bg-[#f8f8fc] w-[80%]">
+        <!-- 导航 -->
+        <div class="nav">
+          <div class="cursor-pointer" @click="toggle()">
+            <Sunny style="width: 1.5em; height: 2em; " v-if="!isDark" />
+            <Moon style="width: 1.5em; height: 2em; " class="bg-black" v-else />
+          </div>
+          <!-- 内容 -->
+          <div class="bg-#f8f8f8">bbbb</div>
+        </div>
+
+      </div>
     </div>
+
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import { useToggle } from '@vueuse/shared'
 import { useDark } from "@vueuse/core";
-import { Sunny, Moon } from '@element-plus/icons-vue'
+import { Sunny, Moon,Plus } from '@element-plus/icons-vue'
 const theme = ref(false)
 const isDark = useDark({
   // 存储到localStorage/sessionStorage中的Key 根据自己的需求更改
