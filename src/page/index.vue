5c547df3eb3a518e587f1f847a06d20e76e4b2a4
lin360024869 <lin360024869@devide>
2023-09-04 12:59:00 +0800

    Mon Sep  4 12:59:00 CST 2023 inscode

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,4 +1,15 @@
-<template>aaaa</template>
+<template>
+  <!--  -->
+  <div class="wrap">
+    <div class="nav"></div>
+    <div class="container">
+      <!-- 左边 -->
+      <div class="aside_left"></div>
+      <!-- 右边 -->
+      <div class="aside_right"></div>
+    </div>
+  </div>
+</template>
 <script setup>
 import { ref } from "vue";
 
@@ -9,7 +20,7 @@ defineProps({
 const count = ref(0);
 </script>
 <style scoped>
-.read-the-docs {
-  color: #888;
+.aside_left{
+  width: ;
 }
 </style>
