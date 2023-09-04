da8254b18df2f3f038b31c48a27674bb17e04d85
lin360024869 <lin360024869@devide>
2023-09-04 17:20:27 +0800

    Auto Commit

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <!--  -->
-  <div class="wrap h-[100vw] bg-red-50">
+  <div class="wrap h-[100vw]">
+    <div class="demo">
+      <div>这是一个深色模式的切换demo</div>
+      <el-switch inline-prompt v-model="theme" @click="toggle()">
+      </el-switch>
+    </div>
     <div class="nav dark"></div>
     <div class="container">
       <!-- 左边 -->
@@ -12,14 +16,20 @@
 </template>
 <script setup>
 import { ref } from "vue";
+import { useToggle } from '@vueuse/shared'
+import { useDark } from "@vueuse/core";
 
-defineProps({
-  msg: String,
-});
+const theme = ref(false)
+const isDark = useDark({
+  // 存储到localStorage/sessionStorage中的Key 根据自己的需求更改
+  storageKey: 'useDarkKEY',
+  // 暗黑class名字
+  valueDark: 'dark',
+  // 高亮class名字
+  valueLight: 'light',
+})
 
-const count = ref(0);
+const toggle = useToggle(isDark);
+
 </script>
-<style scoped>
-.aside_left{
-}
-</style>
+<style scoped></style>
